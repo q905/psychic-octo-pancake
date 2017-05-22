a809b0ab991cccc1759cbf20ee484939de689207
q905 <gmc@hotbox.ru>
2017-05-22 12:53:13 +0400

    fc

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,48 @@
+﻿<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+{
+    // Создаём объект нашей модели
+    $movies = new Application_Model_DbTable_Movies();
+
+    // Применяем метод fetchAll для выборки всех записей из таблицы,
+    // и передаём их в view, через следующую запись
+    $this->view->movies = $movies->fetchAll();
+
+	$exp = "Экспериментальная";
+	$this->view->exp = $exp;
+}
+
+    public function addAction()
+    {
+        // action body
+
+    }
+
+    public function editAction()
+    {
+        // action body
+    }
+
+    public function deleteAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
